49bb4fe8c7b42a1f4c9a6752dda17db4f6bade4d
Vitalii Honchar <weaxme@gmail.com>
2022-11-17 09:04:25 -0500

    Added methods for handle requests

--- internal/infra/web/http_server.go
+++ internal/infra/web/http_server.go
@@ -1,36 +1,70 @@
 package web
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"remoteworkout/internal/infra/web/route"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type HttpServer struct {
 	Origin *http.ServeMux
 }
 
-func (hs *HttpServer) Get(path string, handler func(http.ResponseWriter, *http.Request)) {
-	hs.Origin.HandleFunc(path, decorateHttpMethod(http.MethodGet, handler))
+type httpEndpoint struct {
+	method  string
+	handler func(*Request, chan Response)
 }
 
-func (hs *HttpServer) Post(path string, handler func(http.ResponseWriter, *http.Request)) {
-	hs.Origin.HandleFunc(path, decorateHttpMethod(http.MethodPost, handler))
+func (hs *HttpServer) Get(path string, handler func(*Request, chan Response)) {
+	hs.Origin.HandleFunc(path, handleDecorator(http.MethodGet, handler))
 }
 
-func (hs *HttpServer) Delete(path string, handler func(http.ResponseWriter, *http.Request)) {
-	hs.Origin.HandleFunc(path, decorateHttpMethod(http.MethodPost, handler))
+func (hs *HttpServer) Post(path string, handler func(*Request, chan Response)) {
+	hs.Origin.HandleFunc(path, handleDecorator(http.MethodGet, handler))
+}
+
+func (hs *HttpServer) Delete(path string, handler func(*Request, chan Response)) {
+	hs.Origin.HandleFunc(path, handleDecorator(http.MethodGet, handler))
 }
 
 func (hs *HttpServer) Listen(port int) {
 	http.ListenAndServe(fmt.Sprintf(":%d", port), hs.Origin)
 }
 
-func decorateHttpMethod(method string, handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func handleDecorator(method string, handler func(*Request, chan Response)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == method {
-			handler(w, r)
+			if req, err := createRequest(r); err != nil {
+				log.Error("Unexpected error during parsing request", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			} else {
+				resp := handleRequest(req, handler)
+				writeResponse(resp, w)
+			}
 		}
+	}
+}
+
+func handleRequest(req *Request, handler func(*Request, chan Response)) *Response {
+	c := make(chan Response)
+	go handler(req, c)
+
+	resp := <-c
+	close(c)
+	return &resp
+}
+
+func writeResponse(r *Response, w http.ResponseWriter) {
+	responseBytes, err := json.Marshal(r.Body)
+	if err != nil {
+		log.Errorf("Unexpected error during serializing response: %v", r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(r.StatusCode)
+		w.Write(responseBytes)
 	}
 }
 
